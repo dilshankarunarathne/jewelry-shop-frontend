32124c0db86dbb23cd07b73e44385a8cd25f45d9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-03 02:43:52 +0530

    Update page.tsx

---
+++ jewelry-shop/app/pawn/page.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from '../../components/Navbar' 
+
+export default function ContactPage() {
+  return (
+    <main>
+      <div className="ContactPage">
+        <Navbar />
+        <h1>Contact Us</h1>
+      </div>
+    </main>
+  )
+}
