b14fbbc0b6e236e4ea9ca7cd31610d386dd10d1a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-03 01:43:18 +0530

    Update AboutPage.tsx

--- jewelry-shop/app/AboutPage.tsx
+++ jewelry-shop/app/AboutPage.tsx
@@ -1,1 +1,12 @@
 import { Navbar } from '../components/Navbar' 
+
+export default function AboutPage() {
+  return (
+    <main>
+      <div className="App">
+        <Navbar />
+        <h1>About</h1>
+      </div>
+    </main>
+  )
+}
