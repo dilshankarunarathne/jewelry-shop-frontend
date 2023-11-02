210fdd8267616e1bf17b34ec8c3e510efc24b67c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-03 02:41:53 +0530

    Create page.tsx

---
+++ jewelry-shop/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from '../../components/Navbar' 
+
+export default function AboutPage() {
+  return (
+    <main>
+      <div className="AboutPage">
+        <Navbar />
+        <h1>About</h1>
+      </div>
+    </main>
+  )
+}
