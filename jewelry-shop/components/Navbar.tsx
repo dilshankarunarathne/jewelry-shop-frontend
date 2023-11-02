053d3232fbfe57936d9a04fa4d876da8fbbe0794
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-02 11:41:42 +0530

    Update Navbar.tsx

--- jewelry-shop/components/Navbar.tsx
+++ jewelry-shop/components/Navbar.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <nav>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      // Add more links as needed
+      <button onClick={() => router.push('/')}>Home</button>
+      <button onClick={() => router.push('/about')}>About</button>
+      // Add more buttons as needed
     </nav>
   )
 }
