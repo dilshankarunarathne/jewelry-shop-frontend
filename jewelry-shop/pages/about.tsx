35960202a82e347ba2ea6a6243c0678e7af64407
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-03 02:27:59 +0530

    Update about.tsx

---
+++ jewelry-shop/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      // Your content here
+    </div>
+  );
+}
+
+export default About;
