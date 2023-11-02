e4e6de2e0a177eccb525c5cab847917c7fe78c1a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-03 01:43:27 +0530

    Update AboutPage.tsx

--- jewelry-shop/app/AboutPage.tsx
+++ jewelry-shop/app/AboutPage.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '../components/Navbar'
 export default function AboutPage() {
   return (
     <main>
-      <div className="App">
+      <div className="AboutPage">
         <Navbar />
         <h1>About</h1>
       </div>
