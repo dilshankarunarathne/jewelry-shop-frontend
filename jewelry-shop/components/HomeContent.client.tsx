fda1bb2233a910227d3348ffb2cab972ee0fbd35
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-02 13:05:33 +0530

    Update HomeContent.client.tsx

--- jewelry-shop/components/HomeContent.client.tsx
+++ jewelry-shop/components/HomeContent.client.tsx
@@ -10,7 +10,8 @@ const slides = [
   },
   { 
     img: '/path/to/image2.jpg', description: 'Description 2' },
-  { img: '/path/to/image3.jpg', description: 'Description 3' }
+  { 
+    img: '/path/to/image3.jpg', description: 'Description 3' }
 ];
 
 export default function HomeContent() {
