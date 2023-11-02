e196300cdd900d21ea92612a43ee7f50d776c426
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-02 15:28:56 +0530

    Update Footer.tsx

--- jewelry-shop/components/Footer.tsx
+++ jewelry-shop/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>Developed by Altier Tech ©</p>
+      <p>Copyright © Altier Tech 2023</p>
       <p>ceo@altier.tech</p>
     </footer>
   );
