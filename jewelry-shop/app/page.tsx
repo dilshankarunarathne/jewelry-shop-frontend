e35fc8dc86bfcc22e29d59945eab7a7941bb81e9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-02 11:40:54 +0530

    Update page.tsx

--- jewelry-shop/app/page.tsx
+++ jewelry-shop/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar' // Adjust the path as needed
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Navbar />
     </main>
   )
 }
