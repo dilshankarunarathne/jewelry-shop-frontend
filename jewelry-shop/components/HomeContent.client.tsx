821e824062bf3093eda2cc8def7fe103ae4cfdab
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-02 13:07:42 +0530

    Update HomeContent.client.tsx

--- jewelry-shop/components/HomeContent.client.tsx
+++ jewelry-shop/components/HomeContent.client.tsx
@@ -47,10 +47,8 @@ export default function HomeContent() {
       <div className="slide">
         <img src={slides[currentSlide].img} alt="Slide" />
         <div>
-          
+
         </div>
-        <h1>{slides[currentSlide].heading}</h1>
-        <p>{slides[currentSlide].description}</p>
       </div>
 
       <button onClick={goRight}>
