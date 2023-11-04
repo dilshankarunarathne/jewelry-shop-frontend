4fa39684d239abe4c9824337d0bae9001b2ea217
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-04 09:27:24 +0530

    Update page.tsx

--- jewelry-shop/app/about/page.tsx
+++ jewelry-shop/app/about/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '../../components/Navbar'
 
 export default function AboutPage() {
   return (
-    <main>
+    <main className="gradient-background">
       <div className="AboutPage">
         <Navbar />
         <h1>About</h1>
