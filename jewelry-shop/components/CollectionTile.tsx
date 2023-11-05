dad198f42c729ac20844ff04bed9b8f07102e6d4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 16:45:38 +0530

    Update CollectionTile.tsx

---
+++ jewelry-shop/components/CollectionTile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright © Altier Tech 2023</p>
+      <p>ceo@altier.tech</p>
+    </footer>
+  );
+};
+
+export default Footer;
