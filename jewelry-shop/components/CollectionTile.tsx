5ed21a0ed359ee985d4d9eff04903a39e16e60f2
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 16:48:12 +0530

    Update CollectionTile.tsx

--- jewelry-shop/components/CollectionTile.tsx
+++ jewelry-shop/components/CollectionTile.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-
+interface CollectionTileProps {
+    name: string;
+    image: string;
+    onButtonClick: () => void;
+  }
 
 const CollectionTile = () => {
   return (
