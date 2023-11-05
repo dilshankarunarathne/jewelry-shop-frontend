a5c8a981feea799d5ab1e2e365e4c45c20ec441d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 17:21:00 +0530

    Update page.tsx

--- jewelry-shop/app/products/page.tsx
+++ jewelry-shop/app/products/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Navbar } from '../../components/Navbar' 
 import CollectionTile from '@/components/CollectionTile';
