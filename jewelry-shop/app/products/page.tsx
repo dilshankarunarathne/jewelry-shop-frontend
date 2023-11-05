6cb19cad96b4ccff3269ecd3ba4a866be77266ec
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 12:45:08 +0530

    Update page.tsx

--- jewelry-shop/app/products/page.tsx
+++ jewelry-shop/app/products/page.tsx
@@ -2,10 +2,7 @@ import { Navbar } from '../../components/Navbar'
 
 export default function AboutPage() {
   return (
-    <main style={{
-      background: 'linear-gradient(to right, transparent, #FFFDCE) !important',
-      minHeight: '100vh'
-    }}>
+    <main>
       <div className="AboutPage">
         <Navbar />
         <h1>About</h1>
