c400851c2045f0b6e3b62c9b6d583959a0108a3d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 16:49:17 +0530

    Update CollectionTile.tsx

--- jewelry-shop/components/CollectionTile.tsx
+++ jewelry-shop/components/CollectionTile.tsx
@@ -9,9 +9,9 @@ interface CollectionTileProps {
 const CollectionTile: React.FC<CollectionTileProps> = ({ name, image, onButtonClick }) => {
     return (
         <div className="collection-tile">
-        <h2>{name}</h2>
-        <img src={image} alt={name} />
-        <button onClick={onButtonClick}>SEE COLLECTION</button>
+            <h2>{name}</h2>
+            <img src={image} alt={name} />
+            <button onClick={onButtonClick}>SEE COLLECTION</button>
         </div>
     );
 };
