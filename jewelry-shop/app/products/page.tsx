839fbebd895f7db2c06e5363a923fbece198861c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 16:44:07 +0530

    Update page.tsx

--- jewelry-shop/app/products/page.tsx
+++ jewelry-shop/app/products/page.tsx
@@ -29,7 +29,8 @@ export default function ProductsPage() {
                 selectedTab === 'best seller' && <div>Best seller jewelry content</div>
                 }
                 {
-                selectedTab === 'new' && <div>New jewelry content</div>}
+                selectedTab === 'new' && <div>New jewelry content</div>
+                }
             </div>
         </div>
       </div>
