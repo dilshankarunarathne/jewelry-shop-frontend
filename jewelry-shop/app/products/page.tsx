5d8f1ae2df8292aaa642c787b47ad1521f0af538
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 12:46:50 +0530

    Update page.tsx

--- jewelry-shop/app/products/page.tsx
+++ jewelry-shop/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '../../components/Navbar'
 export default function AboutPage() {
   return (
     <main>
-      <div className="AboutPage">
+      <div className="ProductsPage">
         <Navbar />
         <h1>Latest Collection</h1>
         <p>try our best collection of rings and necklace</p>
