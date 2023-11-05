0f60806c45ca0c83d6985ed3385c423781cd4b29
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-05 16:45:52 +0530

    Update CollectionTile.tsx

--- jewelry-shop/components/CollectionTile.tsx
+++ jewelry-shop/components/CollectionTile.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 const CollectionTile = () => {
   return (
-    <footer>
-      <p>Copyright © Altier Tech 2023</p>
-      <p>ceo@altier.tech</p>
-    </footer>
+    <main>
+        
+    </main>>
   );
 };
 
