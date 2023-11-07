fb43d4e5599ecb21b3c3beae3e7477ce8ac0869b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-07 10:27:44 +0530

    Update mailService.ts

--- jewelry-shop/app/services/mailService.ts
+++ jewelry-shop/app/services/mailService.ts
@@ -1,22 +1,22 @@
 import nodemailer from 'nodemailer';
 
-export async function sendEmail(to: string, subject: string, text: string): Promise<void> {
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-password'
-    }
-  });
-
-  let mailOptions = {
-    from: 'your-email@gmail.com',
-    to: to,
-    subject: subject,
-    text: text
-  };
-
-  let info = await transporter.sendMail(mailOptions);
-
-  console.log('Message sent: %s', info.messageId);
+export async function sendEmail(to: string, subject: string, html: string): Promise<void> {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'your-email@gmail.com',
+        pass: 'your-password'
+      }
+    });
+  
+    let mailOptions = {
+      from: 'your-email@gmail.com',
+      to: to,
+      subject: subject,
+      html: html
+    };
+  
+    let info = await transporter.sendMail(mailOptions);
+  
+    console.log('Message sent: %s', info.messageId);
 }
