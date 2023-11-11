f1313de70d6a6df8375848f931eaae6336f93fa4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 20:10:06 +0530

    Update Navbar.tsx

--- jewelry-shop/components/Navbar.tsx
+++ jewelry-shop/components/Navbar.tsx
@@ -1,46 +1,56 @@
 "use client"
 
 import { useState } from 'react';
-import Link from 'next/link';
+import { Button, Link, Menu, MenuItem, Typography } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import Image from "next/image";
 
 import './Navbar.css'
 
-interface NavigationProps {
-  href: string;
-  children: React.ReactNode;
-}
-
-function Navigation({ href, children }: NavigationProps) {
-  return (
-    <Link href={href}>{children}</Link>
-  );
-}
-
 export function Navbar() {
   const [isActive, setIsActive] = useState(false);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const toggleActive = () => {
     setIsActive(!isActive);
   };
 
   return (
-    <nav className={isActive ? 'active' : ''}>
-      <button onClick={toggleActive} className="menu-button">Menu</button>
-      <Link href="/">
-        <img src="/path/to/logo.png" alt="Shop Logo" className="logo" />
-      </Link>
-      <div>
-        <Link href="/">HOME</Link>
-        <div className="dropdown">
-          <button className="dropbtn">PRODUCTS</button>
-          <div className="dropdown-content">
-            <Navigation href="/products/product1">Product 1</Navigation>
-            <Navigation href="/products/product2">Product 2</Navigation>
-            <Link href="/products">All Products</Link>
-          </div>
-        </div>
-        <Link href="/about">ABOUT US</Link>
-        <Link href="/contact">CONTACT US</Link>
+    <nav>
+      <RouterLink to="/">
+        <Image src="/path/to/logo.png" alt="Shop Logo" className="logo" width={500} height={300} />
+      </RouterLink>
+      <div className="navbar-items">
+        <RouterLink to="/"><Typography variant="h6">HOME</Typography></RouterLink>
+        <RouterLink to="/products">
+          <Typography variant="h6">
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+              PRODUCTS
+            </Button>
+          </Typography>
+        </RouterLink>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}><RouterLink to="/products/product1">Product 1</RouterLink></MenuItem>
+          <MenuItem onClick={handleClose}><RouterLink to="/products/product2">Product 2</RouterLink></MenuItem>
+          <MenuItem onClick={handleClose}><RouterLink to="/products">All Products</RouterLink></MenuItem>
+        </Menu>
+        <RouterLink to="/about"><Typography variant="h6">ABOUT US</Typography></RouterLink>
+        <RouterLink to="/contact"><Typography variant="h6">CONTACT US</Typography></RouterLink>
       </div>
     </nav>
   );
