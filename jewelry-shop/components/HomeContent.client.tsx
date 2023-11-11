da52e6a676d1a1afbacf004ec0042ef5c8086919
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-11 19:06:54 +0530

    Update HomeContent.client.tsx

--- jewelry-shop/components/HomeContent.client.tsx
+++ jewelry-shop/components/HomeContent.client.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { useState, useEffect } from 'react';
-// import './HomeContent.client.css'; 
+
 import { Box, Button, IconButton, Typography } from '@material-ui/core';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
+// import './HomeContent.client.css'; 
 
 const slides = [
   { 
