010073467281b7aef3baadd7069cbe3f3e60faa3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-13 03:38:34 +0530

    pawning page styles

--- jewelry-shop/app/pawn/page.tsx
+++ jewelry-shop/app/pawn/page.tsx
@@ -6,74 +6,67 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import "./pawn.css"
 
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    body: {
-      backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.76), rgba(0, 0, 0, 0.76)), url("/rings-bg.png")',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-    },
-  },
-  h1: {
-    color: 'white',
-    fontFamily: 'Gudea',
-    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
-  },
-  root: {
-    marginTop: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  button: {
-    margin: theme.spacing(3, 0, 2),
-    background: '#FFE55E',
-    fontFamily: 'Inria Sans',
-    color: 'black',
-    // fontWeight: 'bold',
-    marginTop: '23px',
-    textAlign: 'center',
-    padding: '6px 12px', 
-    width: '200px',
-  },
-  subh: {
-    color: 'white',
-    fontFamily: 'Inter',
-    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
-  },
-  text: {
-    color: 'white',
-    fontFamily: 'Gudea',
-    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
-  },
-
-  h1p2: {
-  },
-  fullScreen: {
-    height: '100vh',
-    width: '100%',
-  }
-}));
+// const useStyles = makeStyles((theme) => ({
+//   '@global': {
+//     body: {
+//       backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.76), rgba(0, 0, 0, 0.76)), url("/rings-bg.png")',
+//       backgroundRepeat: 'no-repeat',
+//       backgroundSize: 'cover',
+//     },
+//   },
+//   h1: {
+//     color: 'white',
+//     fontFamily: 'Gudea',
+//     textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+//   },
+//   root: {
+//     marginTop: theme.spacing(4),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   button: {
+//     margin: theme.spacing(3, 0, 2),
+//     background: '#FFE55E',
+//     fontFamily: 'Inria Sans',
+//     color: 'black',
+//     // fontWeight: 'bold',
+//     marginTop: '23px',
+//     textAlign: 'center',
+//     padding: '6px 12px', 
+//     width: '200px',
+//   },
+//   subh: {
+//     color: 'white',
+//     fontFamily: 'Inter',
+//     textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+//   },
+//   text: {
+//     color: 'white',
+//     fontFamily: 'Gudea',
+//     textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+//   },
+// }));
 
 export default function PawnPage() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <Router>      
       <CssBaseline />
-        <Navbar />
-        <Box className={classes.root}>
-        <Typography className={classes.h1} variant="h1">40% Discount</Typography>
-        <Typography className={classes.subh} variant="body1">for all in every pawning of gold articles</Typography>
-        <Typography className={classes.text} variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod</Typography>
-        <Button variant="contained" color="primary" className={classes.button}>
+      <Navbar />
+      <Box className="root">
+        <Typography className="h1" variant="h1">40% Discount</Typography>
+        <Typography className="subh" variant="body1">for all in every pawning of gold articles</Typography>
+        <Typography className="text" variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod</Typography>
+        <Button variant="contained" color="primary" className="button">
           DISCOVER MORE
         </Button>
-        </Box>
-        <Box className={classes.fullScreen}>
-          <Typography className={classes.h1p2} variant="h1">Our Services</Typography>
-          {/* Add your new section content here */}
-        </Box>
-  </Router>
+      </Box>
+      <Box className="fullScreen marginTop">
+        <Typography className="h1p2" variant="h1">Our Services</Typography>
+        {/* Add your new section content here */}
+      </Box>
+    </Router>
   );
 }
