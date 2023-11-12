80ff26b9a0f7b8cff826cd7a7bb2c941bf445096
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-13 04:00:28 +0530

    Update page.tsx

--- jewelry-shop/app/pawn/page.tsx
+++ jewelry-shop/app/pawn/page.tsx
@@ -90,7 +90,7 @@ export default function PawnPage() {
           <Box className="serviceBox">
             <Typography variant="h1">Service 4</Typography>
             <Typography className="sbp" variant="body1">
-              
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
             </Typography>
             <Button className="button" variant="contained" color="primary">Action</Button>
           </Box>
