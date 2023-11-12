f2e52822fc0f9a63472fff86d5853906d4fc6bb6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-12 20:51:13 +0530

    Update page.tsx

--- jewelry-shop/app/about/page.tsx
+++ jewelry-shop/app/about/page.tsx
@@ -1,15 +1,21 @@
-import { Navbar } from '../../components/Navbar' 
+"use client"
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Container, Box, Typography } from '@material-ui/core';
+import { Navbar } from '../../components/Navbar';
 
 export default function AboutPage() {
   return (
-    <main style={{
-      background: 'linear-gradient(to right, transparent, #FFFDCE) !important',
-      minHeight: '100vh'
-    }}>
-      <div className="AboutPage">
-        <Navbar />
-        <h1>About</h1>
-      </div>
-    </main>
-  )
+    <Router>
+      <Container style={{
+        background: 'linear-gradient(to right, transparent, #FFFDCE) !important',
+        minHeight: '100vh'
+      }}>
+        <Box className="AboutPage">
+          <Navbar />
+          
+        </Box>
+      </Container>
+    </Router>
+  );
 }
