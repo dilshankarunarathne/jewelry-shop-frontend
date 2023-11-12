dde219a43e6678f08c34bbbfe8fe2092e3c11aa4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-12 21:07:22 +0530

    Update page.tsx

--- jewelry-shop/app/about/page.tsx
+++ jewelry-shop/app/about/page.tsx
@@ -1,19 +1,25 @@
 "use client"
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Container, Box, Typography } from '@material-ui/core';
+import { Container, Box, Typography, makeStyles } from '@material-ui/core';
 import { Navbar } from '../../components/Navbar';
 
+const useStyles = makeStyles({
+  container: {
+    background: 'linear-gradient(to right, transparent, #FFFDCE) !important',
+    minHeight: '100vh',
+  },
+});
+
 export default function AboutPage() {
+  const classes = useStyles();
+
   return (
     <Router>
-      <Container style={{
-        background: 'linear-gradient(to right, transparent, #FFFDCE) !important',
-        minHeight: '100vh'
-      }}>
+      <Container className={classes.container}>
         <Box className="AboutPage">
           <Navbar />
-          
+          {/* Rest of your code */}
         </Box>
       </Container>
     </Router>
